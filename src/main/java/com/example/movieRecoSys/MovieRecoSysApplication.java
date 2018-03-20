54b1c7cb04a064d7b1a2e27dc16ca010f1c28606
Aleksander Giera <aleksander.giera@protonmail.com>
2018-03-20 13:48:56 +0100

    initial commit

---
+++ src/main/java/com/example/movieRecoSys/MovieRecoSysApplication.java
@@ -0,0 +1,12 @@
+package com.example.movieRecoSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecoSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecoSysApplication.class, args);
+	}
+}
