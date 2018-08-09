fb6a7508d082fb1a8d6fcb997e019ed5d2336f49
Aleksander Giera <aleksander.giera@protonmail.com>
2018-08-09 17:47:15 +0200

    TEMPOTEMPORARILY permiting any requests

--- src/main/java/com/example/movieRecoSys/credential/jwt/WebSecurity.java
+++ src/main/java/com/example/movieRecoSys/credential/jwt/WebSecurity.java
@@ -2,6 +2,7 @@ package com.example.movieRecoSys.credential.jwt;
 
 import com.example.movieRecoSys.credential.service.UserDetailsServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -35,14 +36,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
-                .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+//                .and()
+//                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+//                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+                //offline spring security to on comment this and uncomment upper
+                .anyRequest().permitAll();
     }
 
     @Override
